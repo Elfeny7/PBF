b21f8b6fadc2031107ae6bdbba6a724825fd6725
elfeny7 <ikmalfaris50@gmail.com>
2024-03-13 09:57:29 +0700

    Prakikum 1

---
+++ pertemuan5/routing-demo/README.md
@@ -0,0 +1,11 @@
+# Ikmal Faris Musyaffa
+# 2141720123 / 06 / TI-3C
+# Laporan Pertemuan 5
+
+## Praktikum 1
+![gambar](images/p1-1.png)
+![gambar](images/p1-2.png)
+## To Do
+### Membuat Halaman Profile dengan Routing
+![gambar](images/p1-5.png)
+![gambar](images/p1-6.png)
---
+++ pertemuan5/routing-demo/src/app/profile/page.tsx
@@ -0,0 +1,13 @@
+export default function Profile() {
+    return (
+        <>
+            <div className="bg-red-400">
+                <h1>My Profile</h1>
+                <h3>Nama : Ikmal Faris Musyaffa</h3>
+                <h3>Kelas : TI-3C</h3>
+                <h3>TTL : Kediri, 07 Oktober 2002</h3>
+                <h3>Alamat : Perumahan Tugurejo Indah G-4, Kab.Kediri</h3>
+            </div>
+        </>
+    );
+}
